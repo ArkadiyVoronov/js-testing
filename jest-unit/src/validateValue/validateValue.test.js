f0f1b2ad2dde9f54c20fccdaf05852cd5114a0b3
voronov <vor.arkadiy@gmail.com>
2023-01-09 00:47:20 +0500

    add more test cases to first test

--- jest-unit/src/validateValue/validateValue.test.js
+++ jest-unit/src/validateValue/validateValue.test.js
@@ -2,4 +2,16 @@ const validateValue = require('./validateValue');
 
 test('Validate value', () => {
    expect(validateValue(50)).toBe(true);
-});
+})
+
+describe('validateValue', () => {
+    test('Correct value', () => {
+        expect(validateValue(50)).toBe(true);
+    })
+    test('Less than correct', () => {
+        expect(validateValue(-1)).toBe(false);
+    })
+    test('More of the correct', () => {
+        expect(validateValue(101)).toBe(false);
+    })
+})
