95af5946878478d23057134c66fba21805f018a9
voronov <vor.arkadiy@gmail.com>
2023-01-09 01:13:05 +0500

    add test cases to second test

--- jest-unit/src/mapArrToSTrings/mapArrToStrigs.test.js
+++ jest-unit/src/mapArrToSTrings/mapArrToStrigs.test.js
@@ -4,5 +4,13 @@ describe('mapArrToStrings', () => {
     test('Correct string', () => {
         expect(mapArrToStrings([1, 2, 3])).toEqual(['1', '2', '3']);
     })
-    
+    test('Mishmash', () => {
+        expect(mapArrToStrings([1, 2, 3, null, undefined, 'strange'])).toEqual(['1', '2', '3']);
+    })
+    test('Empty array', () => {
+        expect(mapArrToStrings([])).toEqual([]);
+    })
+    test('Negative', () => {
+        expect(mapArrToStrings([1, 2, 3])).not.toEqual(['1', '2', '3', '4']);
+    })
 })
