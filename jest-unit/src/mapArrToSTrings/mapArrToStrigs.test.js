9a52d93ddfe4357ea0107002654c12ce90a1f547
voronov <vor.arkadiy@gmail.com>
2023-01-09 01:07:25 +0500

    fix file name to fix test

--- jest-unit/src/mapArrToSTrings/mapArrToStrigs.test.js
+++ jest-unit/src/mapArrToSTrings/mapArrToStrigs.test.js
@@ -2,18 +2,7 @@ const mapArrToStrings = require('./mapArrToStrings');
 
 describe('mapArrToStrings', () => {
     test('Correct string', () => {
-        expect(mapArrToStrings([1, 2, 3])).toBe(['1', '2', '3']);
+        expect(mapArrToStrings([1, 2, 3])).toEqual(['1', '2', '3']);
     })
-    test('Less than correct', () => {
-        expect(mapArrToStrings(-1)).toBe(false);
-    })
-    test('More of the correct', () => {
-        expect(mapArrToStrings(101)).toBe(false);
-    })
-    test('Boundary value from the bottom', () => {
-        expect(mapArrToStrings(0)).toBe(true);
-    })
-    test('Boundary value from the upper', () => {
-        expect(mapArrToStrings(100)).toBe(true);
-    })
+    
 })
