19d55d0ada3f882f6af1ba5919a436621439d416
voronov <vor.arkadiy@gmail.com>
2023-01-09 01:18:42 +0500

    add func square

---
+++ jest-unit/src/square/square.tes.js
@@ -0,0 +1,9 @@
+const square = require('./square');
+
+describe('square', () => {
+    test('Correct value', () => {
+        expect(square(2)).toBe(4);
+        expect(square(2)).toBeLessThan(5);
+        expect(square(2)).toBeGreaterThan(3);
+    })
+})
