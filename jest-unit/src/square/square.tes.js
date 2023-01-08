0ccbcc6689a92f17a3f062248b8ceba7ed0d1f25
voronov <vor.arkadiy@gmail.com>
2023-01-09 01:21:42 +0500

    add not underfined test case

--- jest-unit/src/square/square.tes.js
+++ jest-unit/src/square/square.tes.js
@@ -5,5 +5,6 @@ describe('square', () => {
         expect(square(2)).toBe(4);
         expect(square(2)).toBeLessThan(5);
         expect(square(2)).toBeGreaterThan(3);
+        expect(square(2)).not.toBeUndefined();
     })
 })
